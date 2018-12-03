3546872d2b3b003b40efe24f6cda117900871125
nilabis <44949324+nilabis@users.noreply.github.com>
2018-12-03 18:37:52 +0100

    Delete modulator_qam.m

--- modulator_qam.m
+++
@@ -1,8 +0,0 @@
-function modulated_signal=modulator_qam(signal_cons,M)
-    decimal_sym=zeros(1,length(signal_cons)/M);
-    for i=1:length(signal_cons)/M
-       decimal_sym(i)=bi2de(signal_cons((i-1)*M+1:i*M));
-    end
-        
-    modulated_signal = qammod(decimal_sym,2^M,'UnitAveragePower',true);
-end
