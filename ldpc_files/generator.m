1c62a43b1b6a9610f99e592f84d026ab6f25ee6e
jomiferto <jomifertorres@gmail.com>
2018-12-08 15:48:53 +0100

    Add files via upload

--- ldpc_files/generator.m
+++ ldpc_files/generator.m
@@ -8,21 +8,23 @@ function codeword  = generator(incoming, c)
     %Linear independent set.
     c = g2rref(c);
     
+    %Removes zero rows.
+    c_reduced = c(any(c,2),:);
+    
     %Determines codeword size.
-    codeword_size = size(c, 2);
+    codeword_size = size(c_reduced, 2);
     
     %Initializes the codeword;
     codeword = zeros(1, codeword_size);
     
     
     %Which lines to add.
-        index = find(incoming);
+    index = find(incoming);
         
     for i = index
         
-        codeword = xor(c(i,:), codeword); 
+        codeword = xor(c_reduced(i,:), codeword); 
         
     end
-
-    
+ 
 end
